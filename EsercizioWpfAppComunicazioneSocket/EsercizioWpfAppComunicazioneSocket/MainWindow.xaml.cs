98ca4a28d56b94d4b012eeaefcfb846b74896e36
Jacopo Bucci <79740705+Jaco-77@users.noreply.github.com>
2021-05-12 13:01:16 +0200

    Aggiunta commenti e bug fix

--- EsercizioWpfAppComunicazioneSocket/EsercizioWpfAppComunicazioneSocket/MainWindow.xaml.cs
+++ EsercizioWpfAppComunicazioneSocket/EsercizioWpfAppComunicazioneSocket/MainWindow.xaml.cs
@@ -24,45 +24,71 @@ namespace EsercizioWpfAppComunicazioneSocket
     /// <summary>
     /// Logica di interazione per MainWindow.xaml
     /// </summary>
+    
+    // Bucci Jacopo 4 L Progetto 1
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
 
+            // Si imposta l'indirizzo ip della macchina attualmente in uso (quello usato è quello di default) e la porta che si vuole utilizzare
             IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 60000);
 
+            // Si dichiara e inizializza il thread per il ricevere i messaggi
             Thread t1 = new Thread(new ParameterizedThreadStart(SocketReceive));
 
+            // Sempre lo stesso thread si fa partire
             t1.Start(localEndPoint);
         }
 
+        /// <summary>
+        /// Metodo async per la ricezione dei mess
+        /// </summary>
+        /// <param name="sourceEndPoint"></param>
         public async void SocketReceive(object sourceEndPoint)
         {
+            // Gli viene assegnato l'IP del destinatario
             IPEndPoint sourceEP = (IPEndPoint)sourceEndPoint;
 
+            // Si dichiara e inizializza il socket che si dovrà utilizzare per la ricezione
             Socket t = new Socket(sourceEP.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
 
+            // 
             t.Bind(sourceEP);
 
+            // Viene impostat la lunghezza massima del messaggio in arrivo
             byte[] byteRicevuti = new byte[256];
 
+            // La stringa di default se il mess è vuoto
             string message = "";
 
+            // Contatore per i bytes ricevuti
             int bytes = 0;
 
+            // Per usare il metodo in parallelo con async
             await Task.Run(() =>
             {
-                while(true)
+                // Ciclo infinito per essere sempre pornti alla ricezione
+                while (true)
                 {
-                    if(t.Available > 0)
+                    // Controlla se t è pronto
+                    if (t.Available > 0)
                     {
+                        // Si cancella il contenuto
                         message = "";
+
+                        // Legge i bytes in ricezione
                         bytes = t.Receive(byteRicevuti, byteRicevuti.Length, 0);
+
+                        // Converte in Ascii i bytes ricevuti
                         message += Encoding.ASCII.GetString(byteRicevuti, 0, bytes);
 
+                        // Se possibile fa scrivere sulla list box il messaggio
                         this.Dispatcher.BeginInvoke(new Action(() =>
                         {
+                            // Aggiunge alla listbox il contenuto del messaggio
                             lblRicezione.Content = message;
                         }));
                     }
@@ -70,18 +96,55 @@ namespace EsercizioWpfAppComunicazioneSocket
             });
         }
 
+        /// <summary>
+        /// Metodo invia del messaggio
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnInvia_Click(object sender, RoutedEventArgs e)
         {
-            IPAddress ipDest = IPAddress.Parse(txtIPAdd.Text);
-            int portDest = int.Parse(txtDestPort.Text);
+            // Se ci sono errori avviso l'utente
+            try
+            {
+                // Controllo se le textbox sono state riempite
+                if (txtDestPort.Text.Length > 0 && txtDestPort.Text.Length > 0 && txtMsg.Text.Length > 0)
+                {
+                    // Converte l'IP del destinatario inserito in un IP
+                    IPAddress ipDest = IPAddress.Parse(txtIPAdd.Text);
 
-            IPEndPoint remoteEndpoint = new IPEndPoint(ipDest, portDest);
+                    // Converte il numero della porta appena inserita
+                    int portDest = int.Parse(txtDestPort.Text);
 
-            Socket s = new Socket(ipDest.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
+                    // Imposta un IPEndPoint per il destinatario
+                    IPEndPoint remoteEndpoint = new IPEndPoint(ipDest, portDest);
 
-            byte[] byteInviati = Encoding.ASCII.GetBytes(txtMsg.Text);
+                    // Si dichiara e inizializza il socket che si dovrà utilizzare per l'invio
+                    Socket s = new Socket(ipDest.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
 
-            s.SendTo(byteInviati, remoteEndpoint);
+                    // Si convertino in byte il messaggio da inviare
+                    byte[] byteInviati = Encoding.ASCII.GetBytes(txtMsg.Text);
+
+                    // Invia il messaggio
+                    s.SendTo(byteInviati, remoteEndpoint);
+
+                    txtMsg.Clear();
+                }
+                else
+                {
+                    // Avviso l'utente
+                    throw new Exception("Errore, inserisci qualcosa");
+                }
+            }
+            // Avviso l'utente se la conversione è andata male
+            catch (FormatException ex)
+            {
+                MessageBox.Show("ERRORE, riprova attenzione ai numeri inseriti\n" + ex.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            // Avviso l'utente se è andato male qualcosa
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERRORE, riprova\n" + ex.Message, "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
